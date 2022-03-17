d9f9f8a781892dcb6012e5129b9ea4e268334d4e
gnos28 <gnos28@gmail.com>
2022-03-17 15:36:33 +0100

    add prettierRC

--- app.js
+++ app.js
@@ -1,49 +1,51 @@
-const burgerMenu = document.getElementById("burger-menu");
-const nav = document.querySelector("nav");
-const main = document.querySelector("main");
-const lis = [...document.querySelectorAll("nav > ul > li")];
-const liLinks = [...document.querySelectorAll("nav > ul > li a")];
+const burgerMenu = document.getElementById('burger-menu')
+const nav = document.querySelector('nav')
+const main = document.querySelector('main')
+const lis = [...document.querySelectorAll('nav > ul > li')]
+const liLinks = [...document.querySelectorAll('nav > ul > li a')]
 
-let burgerShow = false;
+let burgerShow = false
 
 const hideBurgerMenu = () => {
-  nav.classList.remove("burgerAnimationIn");
-  main.classList.remove("blur");
+  nav.classList.remove('burgerAnimationIn')
+  main.classList.remove('blur')
 
   let timer = window.setTimeout(() => {
-    nav.classList.remove("burgerShow");
-  }, 400);
+    nav.classList.remove('burgerShow')
+  }, 400)
 
-  lis.forEach((li) => li.classList.remove("liAnimationIn"));
-  burgerShow = false;
+  lis.forEach((li) => li.classList.remove('liAnimationIn'))
+  burgerShow = false
 
-  console.log("hide burger Menu")
-};
+  console.log('hide burger Menu')
+}
 
-burgerMenu.addEventListener("click", (e) => {
-  e.preventDefault();
+burgerMenu.addEventListener('click', (e) => {
+  e.preventDefault()
 
   if (!burgerShow) {
-    nav.classList.add("burgerShow");
-    main.classList.add("blur");
+    nav.classList.add('burgerShow')
+    main.classList.add('blur')
     let timer = window.setTimeout(
-      () => nav.classList.add("burgerAnimationIn"),
+      () => nav.classList.add('burgerAnimationIn'),
       10
-    );
+    )
     timer = window.setTimeout(() => {
       lis.forEach((li, index) => {
         let subTimer = window.setTimeout(() => {
-          li.classList.add("liAnimationIn");
-        }, 50 * index);
-      });
-    }, 100);
+          li.classList.add('liAnimationIn')
+        }, 50 * index)
+      })
+    }, 100)
 
-    burgerShow = true;
+    burgerShow = true
   } else hideBurgerMenu()
 
   // nav.classList.toggle("burgerAnimation")
-});
+})
 
-liLinks.forEach((link) => link.addEventListener("click", (e) => {
-  hideBurgerMenu()
-}));
+liLinks.forEach((link) =>
+  link.addEventListener('click', (e) => {
+    hideBurgerMenu()
+  })
+)
