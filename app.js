649ac89ee0374f89f7e92a0343e9d53e931f5542
gnos28 <gnos28@gmail.com>
2022-03-17 06:58:06 +0100

    burger menu animation

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
-const burgerMenu = document.getElementById("burger-menu")
-const nav = document.querySelector("nav")
+const burgerMenu = document.getElementById("burger-menu");
+const nav = document.querySelector("nav");
+const main = document.querySelector("main");
+const lis = [...document.querySelectorAll("nav > ul > li")];
 
-let burgerShow = false
+let burgerShow = false;
 
-burgerMenu.addEventListener("click",(e) => {
-    e.preventDefault()
+burgerMenu.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    if(!burgerShow)
-    {
-        nav.classList.add("burgerShow")
-        nav.classList.add("burgerAnimationIn")
-        burgerShow = true
-    }
-    else
-    {
-        nav.classList.remove("burgerShow")
-        nav.classList.remove("burgerAnimationIn")
-        burgerShow = false
-    }
+  if (!burgerShow) {
+    nav.classList.add("burgerShow");
+    main.classList.add("blur");
+    let timer = window.setTimeout(
+      () => nav.classList.add("burgerAnimationIn"),
+      10
+    );
+    timer = window.setTimeout(() => {
+      lis.forEach((li, index) => {
+        let subTimer = window.setTimeout(
+          () => {
+                li.classList.add("liAnimationIn");
+            },
+          50*index
+        );
+      });
+    }, 400);
 
-    // nav.classList.toggle("burgerAnimation")
-})
+    burgerShow = true;
+  } else {
+    nav.classList.remove("burgerShow");
+    main.classList.remove("blur");
+    nav.classList.remove("burgerAnimationIn");
+    lis.forEach((li) => li.classList.remove("liAnimationIn"));
+    burgerShow = false;
+  }
+
+  // nav.classList.toggle("burgerAnimation")
+});
